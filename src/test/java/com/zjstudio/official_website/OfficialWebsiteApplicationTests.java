b6612f82ae6d7d328636211a3fe7275853ec8559
wa_ta <2647001559@qq.com>
2020-02-14 17:08:30 +0800

    初始化项目结构

---
+++ src/test/java/com/zjstudio/official_website/OfficialWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjstudio.official_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
